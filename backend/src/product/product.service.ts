4fd716d8e907a5eb5ad0b0bb7f44bbf637a5630e
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-01-02 18:06:41 +0100

    add: ProductService with create, findAll and findOne methods

---
+++ backend/src/product/product.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/services/prisma.service';
+import { CreateProductDto } from './dto/create-product.dto';
+
+@Injectable()
+export class ProductsService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: CreateProductDto) {
+    return this.prisma.product.create({ data });
+  }
+
+  async findAll() {
+    return this.prisma.product.findMany();
+  }
+
+  async findOne(id: string) {
+    const product = await this.prisma.product.findUnique({ where: { id } });
+    if (!product)
+      throw new NotFoundException(`Product with id ${id} not found`);
+    return product;
+  }
+}
