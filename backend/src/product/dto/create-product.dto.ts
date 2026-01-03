6d677c568ef39a70a9d5160554268932b271922a
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-01-03 18:35:38 +0100

    add: validation for create product dtos

--- backend/src/product/dto/create-product.dto.ts
+++ backend/src/product/dto/create-product.dto.ts
@@ -1,6 +1,16 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
   title!: string;
+
+  @IsString()
   description!: string;
+
+  @IsNumber()
   price!: number;
+
+  @IsString()
   image!: string;
 }
