f20c343709d2d6cf1c703e6b8c001848dfb2910f
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-01-04 16:57:48 +0100

    feat: store.js and axios.js with config

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,13 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import { Provider } from 'react-redux';
+import store from './app/store.js';
+import { BrowseRouter } from 'react-router-dom';
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById('root')).render(
+  <BrowseRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowseRouter>
+);
