164962d0681cb1e12543f98a07d59b78ad1327e9
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-01-04 20:13:47 +0100

    fix: build dist and connected back with front

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { LoggerInterceptor } from './shared/interceptor/logger.interceptor';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalInterceptors(new LoggerInterceptor());
   app.setGlobalPrefix('api');
   app.enableCors({
@@ -14,8 +16,21 @@ async function bootstrap() {
     methods: 'GET,POST,PUT,PATCH,DELETE',
     credentials: true,
   });
-  await app.enableShutdownHooks();
 
+  const clientPath = join(process.cwd(), 'client');
+
+  app.use(express.static(clientPath));
+  app.use((req, res, next) => {
+    if (req.path.startsWith('/api')) {
+      return next();
+    }
+
+    res.sendFile(join(clientPath, 'index.html'));
+  });
+
+  await app.enableShutdownHooks();
   await app.listen(8000);
+
+  console.log('🚀 Server running on http://localhost:8000');
 }
 bootstrap();
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  baseURL: import.meta.env.PROD ? '/api' : 'http://localhost:8000/api',
 });
