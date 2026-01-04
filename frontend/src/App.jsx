76ace976a8ca4c2dfef064dfdc21975c01305312
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-01-04 18:18:53 +0100

    add: Route in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,20 @@
+import { Link, Route, BrowserRouter, Routes } from 'react-router-dom';
+import Cart from './pages/Cart/Cart';
+import Home from './pages/Home/Home';
+
 function App() {
-  return;
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/cart'>Cart</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
